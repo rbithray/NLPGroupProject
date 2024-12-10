6ee1d5e123b699637b53e69378d504fda9e93811
jkfwensink <jiswiwensink11@gmail.com>
2024-12-10 15:50:16 +0100

    Q1 fit BERT

--- research q1.py
+++ research q1.py
@@ -8,3 +8,8 @@ from os import path
 comments = pd.read_json(path.join('data', 'comments.ndjson'), lines=True)
 submissions = pd.read_json(path.join('data', 'submissions.ndjson'), lines=True)
 
+model = BERTopic()
+
+topics, probs = model.fit_transform(comments['body'])
+print(model.get_topic_info())
+model.get_topic(0)

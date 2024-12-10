3574044cc1dd8628ce8c820ce664589d2c3eb2c5
jkfwensink <jiswiwensink11@gmail.com>
2024-12-10 15:02:27 +0100

    dkdks

--- research q1.py
+++ research q1.py
@@ -3,6 +3,16 @@ from bertopic import BERTopic
 import torch 
 import pandas as pd
 import json
+import os
 
+import json
 
-# NLPGroupProject\data\comments.ndjson
+# Open the NDJSON file
+from os import path:
+
+with open('data\comments.ndjson', 'r') as f:
+    data = [json.loads(line) for line in f]
+
+# `data` is now a list of dictionaries
+for item in data:
+    print(item)

81b2092a0954001bb23cd8354ff4c30289d63789
jkfwensink <jiswiwensink11@gmail.com>
2024-12-10 15:16:18 +0100

    ndsnsd

--- research q1.py
+++ research q1.py
@@ -8,4 +8,3 @@ from os import path
 comments = pd.read_json(path.join('data', 'comments.ndjson'), lines=True)
 submissions = pd.read_json(path.join('data', 'submissions.ndjson'), lines=True)
 
-print(comments.columns)

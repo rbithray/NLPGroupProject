d50a5e63ec2a5fed7ffb576e15d239e8abf6e0d3
blotebaars <blotebaars@gmail.com>
2024-12-13 12:13:33 +0100

    Visualized topics over time

--- RQ2.py
+++ RQ2.py
@@ -1,3 +1,4 @@
+#%%
 from bertopic import BERTopic
 import numpy as np
 import pandas as pd
@@ -8,22 +9,24 @@ comments_path = 'data/comments.ndjson'
 submissions_path = 'data/submissions.ndjson'
 
 
-# comment_data = pd.read_json(path.join('data', 'comments.ndjson'), lines=True)
-# submission_data = pd.read_json(path.join('data', 'comments.ndjson'), lines=True)
-def load_comment_data(path):
-    comment_data = []
-    with open(comments_path, 'r') as f:
-        for line in f:
-            data = json.loads(line)
-    if data['body'] != '[removed]' or data['body'] != '[deleted]':
-        comment_data.append(data['body'])
-    
-    return comment_data
+comments_df = pd.read_json(path.join('data', 'comments.ndjson'), lines=True)
+submission_data = pd.read_json(path.join('data', 'comments.ndjson'), lines=True)
 
-comment_data = load_comment_data(comments_path)
 
+comments = comments_df[(comments_df['body'] != '[removed]') & (comments_df['body'] != '[deleted]')]
+comments['date'] = pd.to_datetime(comments['created_utc'], unit='s').dt.strftime("%d, %m, %Y")
+print(comments['date'])
+#%%
+# Initializing and fitting the model
 model = BERTopic()
-# print(comment_data['body'][:10])
-topics, probs = model.fit_transform(comment_data['body'])
-model.visualize_topics()
+topics, probs = model.fit_transform(comments['body'])
 
+#%%
+# model.visualize_topics()
+# model.get_topic_info()
+topics_over_time = model.topics_over_time(comments['body'], comments['date'], nr_bins=7, datetime_format='%d, %m, %Y')
+model.visualize_topics_over_time(topics_over_time, top_n_topics=10)
+
+
+
+# %%

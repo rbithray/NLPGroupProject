3bbaf0c8ca1cb8653c129e47c87706c8ee9a6b20
blotebaars <blotebaars@gmail.com>
2025-01-08 18:48:23 +0100

    custom topic representation labels

--- RQ2.py
+++ RQ2.py
@@ -1,9 +1,12 @@
 #%%
+!pip install --upgrade nbformat
+#%%
 from bertopic import BERTopic
 import numpy as np
 import pandas as pd
 import json
 from os import path
+import nbformat
 
 comments_path = 'data/comments.ndjson'
 submissions_path = 'data/submissions.ndjson'
@@ -24,8 +27,27 @@ topics, probs = model.fit_transform(comments['body'])
 #%%
 # model.visualize_topics()
 # model.get_topic_info()
+
+new_topic_labels = {
+    0: "AI and Humanity",
+    1: "Art and copyright",
+    2: "AI and consciousness",
+    3: "Chatbots and conversation",
+    4: "AI agents and tools"
+}
+
+
 topics_over_time = model.topics_over_time(comments['body'], comments['date'], nr_bins=7, datetime_format='%d, %m, %Y')
-model.visualize_topics_over_time(topics_over_time, top_n_topics=10)
+model.set_topic_labels(new_topic_labels)
+
+model.visualize_topics_over_time(topics_over_time, custom_labels=new_topic_labels, top_n_topics=5)
+
+# model.visualize_topics_over_time(topics_over_time, top_n_topics=10)
+
+
+
+# %%
+print(model.topic_representations_)
 
 
 

9fb3d59d6a377aa1c785b8d645af63b35d923cb8
jkfwensink <jiswiwensink11@gmail.com>
2025-01-10 11:39:31 +0100

    jdnjs

--- preprocess.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-from os import path
-
-comments = pd.read_json(path.join('data', 'comments.ndjson'), lines=True)
-submissions = pd.read_json(path.join('data', 'submissions.ndjson'), lines=True)
